d6f565d5e1ffa4463abadc0e2dcd7568c920fc20
SYNNYCUN <shinnosuke.kajiwara@gmail.com>
2019-06-04 16:44:55 -0600

    sdf

--- LearningPython/ShinKattisProblems/Skocimis.py
+++ LearningPython/ShinKattisProblems/Skocimis.py
@@ -1,1 +1,10 @@
-x, y, z = input().split()
+import math
+line = input().split()
+x, y, z = int(line[0]), int(line[1]), int(line[2])
+
+dif1 = y-x
+dif2 = z-y
+if dif1 > dif2:
+    print(dif1-1)
+else:
+    print(dif2-1)
