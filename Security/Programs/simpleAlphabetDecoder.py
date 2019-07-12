034d530fdc32966a3fd5f1dfac8740d51a462e96
ndbellew <superdaggler68@gmail.com>
2019-07-12 17:18:50 -0600

    update now if you are given letters or numbers you will get a worded response.

--- Security/Programs/simpleAlphabetDecoder.py
+++ Security/Programs/simpleAlphabetDecoder.py
@@ -14,6 +14,7 @@ usage = "Usage: %prog [-o | --offset N] [letters]"
 parser = OptionParser(usage)
 parser.add_option('-o','--offset', dest="offset", help="This cipher will use a simple offset, it will alwyas default to 0. simple add -o and then any number such as \n -o 4\n to set number 1 to 5 and then translate to letters")
 parser.add_option('-v','--version', dest="version", action="store_true", help="if added will print the Version of the program")
+parser.add_option('-n', '--numbers', dest ="numbers", action="store_true", help="Works just like normal except if you are given the numbers of the alphabet instead of the letters.")
 (options, args)= parser.parse_args()
 
 if len(args) < 1 or options.version:
@@ -30,17 +31,34 @@ def FindLetter(letter):
     for i in range(0, len(alpha)-1):
         if letter == alpha[i]:
             return i
-def main():
+def Letters():
     EncryptedPhrase = " ".join(args)
     DecryptedPhrase = ""
     for i in EncryptedPhrase:
         if i != " ":
             DL = int(offset)+FindLetter(i)
-            if DL>25:
+            while DL>25:
                 DL-=26
             DecryptedPhrase+= alpha[DL]
         else:
             DecryptedPhrase+= " "
     print(DecryptedPhrase)
+
+def Numbers():
+    EncryptedPhrase = args
+    DecryptedPhrase = ""
+    for i in EncryptedPhrase:
+        if i!=" ":
+            DL = int(offset)+int(i)-1
+            while DL>25:
+                DL-=26
+            DecryptedPhrase+= alpha[DL]
+    print(DecryptedPhrase)
+
+def main():
+    if options.numbers:
+        Numbers()
+    else:
+        Letters()
 
 main()
