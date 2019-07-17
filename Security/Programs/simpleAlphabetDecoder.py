21e053eacaa63a56aeb6ca00db8a21cdda1e2fb0
ndbellew <superdaggler68@gmail.com>
2019-07-17 14:40:01 -0600

    Added Uppercase and bug fixes. V0.3 Alpha well the alpha is vinally on 0.3 with some more functionality and a some big bug fixes, this is essentially a Caesar Cipher.

--- Security/Programs/simpleAlphabetDecoder.py
+++ Security/Programs/simpleAlphabetDecoder.py
@@ -15,6 +15,7 @@ parser = OptionParser(usage)
 parser.add_option('-o','--offset', dest="offset", help="This cipher will use a simple offset, it will alwyas default to 0. simple add -o and then any number such as \n -o 4\n to set number 1 to 5 and then translate to letters")
 parser.add_option('-v','--version', dest="version", action="store_true", help="if added will print the Version of the program")
 parser.add_option('-n', '--numbers', dest ="numbers", action="store_true", help="Works just like normal except if you are given the numbers of the alphabet instead of the letters.")
+parser.add_option('-u', action="store_true", dest="uppercase",help="This will return the message in all uppercase.")
 (options, args)= parser.parse_args()
 
 if len(args) < 1 or options.version:
@@ -28,7 +29,8 @@ if len(args) < 1 or options.version:
 if options.offset:
     offset = int(options.offset)
 def FindLetter(letter):
-    for i in range(0, len(alpha)-1):
+    for i in range(0, len(alpha)):
+
         if letter.lower() == alpha[i]:
             return i
 def Letters():
@@ -36,13 +38,14 @@ def Letters():
     DecryptedPhrase = ""
     for i in EncryptedPhrase:
         if i != " ":
+
             DL = offset+int(FindLetter(i))
             while DL>25:
                 DL-=26
             DecryptedPhrase+= alpha[DL]
         else:
             DecryptedPhrase+= " "
-    print(DecryptedPhrase)
+    PrintDecryptedPhrase(DecryptedPhrase)
 
 def Numbers():
     EncryptedPhrase = args
@@ -53,7 +56,13 @@ def Numbers():
             while DL>25:
                 DL-=26
             DecryptedPhrase+= alpha[DL]
-    print(DecryptedPhrase)
+    PrintDecryptedPhrase(DecryptedPhrase)
+
+def PrintDecryptedPhrase(DP):
+    if options.uppercase:
+        print(DP.upper())
+    else:
+        print(DP.lower())
 
 def main():
     if options.numbers:
